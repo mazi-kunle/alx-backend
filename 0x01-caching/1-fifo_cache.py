71d8438793ea97fb9c7d2f30d69a61fe7241a80b
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-06 20:18:32 +0100

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,19 +18,19 @@ class FIFOCache(BaseCaching):
                 # check if key exists, remove from queue if true.
                 if (key in self.cache_data):
                     self.arr.pop(self.arr.index(key))
-                   
-                self.arr.append(key) # add to top of queue
+
+                self.arr.append(key)  # add to top of queue
                 self.cache_data[key] = item
             else:
                 # check if key exists, if true remove from queue
                 if (key in self.cache_data):
                     self.arr.pop(self.arr.index(key))
-                  
+
                 else:
                     removed_key = self.arr.pop(0)
                     del self.cache_data[removed_key]
                     print(f'DISCARD: {removed_key}')
-           
+
                 # replacement policy
                 self.arr.append(key)
                 self.cache_data[key] = item
