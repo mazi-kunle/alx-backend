014a433f80f4df84578a9929eaefd816d4ad575f
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-13 14:58:27 +0100

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,9 +3,22 @@
 
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    '''config class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+# print(app.config)
 
 
 @app.route('/')
