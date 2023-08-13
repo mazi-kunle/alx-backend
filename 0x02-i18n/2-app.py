d16929543fe76474eb546d4455d2470af85a0676
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-13 15:04:18 +0100

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''This is a flask module'''
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    '''config class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+# print(app.config)
+
+
+@app.route('/')
+def hello_world():
+    '''
+    render a simple hello world page
+    '''
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    '''get best locale'''
+    return request.acccept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
